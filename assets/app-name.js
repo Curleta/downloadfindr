14cfcff9f14d6b24c7c22b1bed91621731738f16
Curleta <hello.curleta@gmail.com>
2025-11-15 23:53:57 +0000

    Updated the loop function to fix bug

--- assets/app-name.js
+++ assets/app-name.js
@@ -2,16 +2,16 @@ var highlightAppName = [ "Valorant", "Clash Royale", "Discord" ];
 var appNameOutput = document.getElementById("highlight-app-name");
 var i = 0;
 
-function changeAppName() {
-    appNameOutput.innerHTML = highlightAppName[i];
-    if(i < highlightAppName.length - 1) {
-        i++;
-    } else {
-        i = 0;
+function wait(ms) {
+    return new Promise(res => setTimeout(res, ms))
+}
+
+async function loop() {
+    while(true) {
+        appNameOutput.innerHTML = highlightAppName[i];
+        i = (i + 1) % highlightAppName.length;
+        await wait(5000);
     }
 }
 
-while (true) {
-    changeAppName();
-    setTimeout("changeAppName", 5000);
-}
+loop();
